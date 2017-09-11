739adcdecd7e2d1d7309019264d11f519c0ab176
oliverlai55 <oliverlai55@gmail.com>
2017-09-11 00:25:51 -0400

    Complete last section

--- elementary-electron/index.js
+++ elementary-electron/index.js
@@ -1,7 +1,24 @@
 var picture = require('cat-picture');
 var image = require('lightning-image-poly');
+var remote = require('electon').remote;
+var fs = require('fs');
 
 var src = picture.src;
 picture.remove();
 
 var viz = new image('#visualization', null, [src], { hullAlgorithm: 'convex' });
+
+function save() {
+	remote.getCurrentWebContents().printToPDF({
+		portrait: true
+	}, function(err, data) {
+		fs.writeFile('annotation.pdf', data, function(err) {
+			if (err) alert('error generating pdf! ' + err.message);
+			else alert('pdf saved!');
+		});
+	});
+}
+
+window.addEventListner('keydown', function(e) {
+	if (e.keyCode == 80) save();
+});
